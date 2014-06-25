a8969555d493b7ca42c00dd55e2d52554950f3ab
Guinsly Mondésir <guinslym@gmail.com>
2014-06-25 00:29:43 -0400

    finish ... need to review the logic of handleNext and handlePrevious

--- tutoriel/ng-animate/animate-1/app.js
+++ tutoriel/ng-animate/animate-1/app.js
@@ -1,5 +1,49 @@
 /* implementing Angular-strap (bootstrap-Modal) */
-angular.module('App', ['$strap.directives']);
+var myApp = angular.module('App', ['$strap.directives']);
 
 
-angular.controller('AppCtrl', function($scope) {});
+myApp.controller('AppCtrl', function($scope) {
+   $scope.steps = ['one', 'two', 'three'];
+   $scope.step = 0;
+   $scope.stepIndex = 0;
+   $scope.wizard = { tacos: 2 };
+
+   $scope.isCurrentStrep = function(step){
+     return $scope.step === step;
+   };
+
+   $scope.setCurrentStep = function(step){
+     return $scope.step = step;
+   };
+
+   $scope.getCurrentStep = function() {
+       return $scope.steps[$scope.step];
+   };
+   
+   $scope.isFirstStep = function() {
+     return $scope.step === 0;
+   };
+
+   $scope.isLastStep = function() {
+     return $scope.step === ($scope.steps.length -1);
+   };
+
+   $scope.getNextLabel = function() {
+     return ($scope.isLastStep()) ? 'Submit' : 'Next';
+    };
+
+   $scope.handlePrevious = function() {
+     $scope.step = ($scope.isFirstStep()) ? 0 : 1;
+   };
+
+   $scope.handleNext = function(dismiss){
+       if($scope.isLastStep()){
+           dismiss();
+       } else {
+           $scope.step += 1;
+       }
+    };
+
+
+    });
+
